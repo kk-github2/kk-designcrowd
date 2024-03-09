b36ac067da461ea247d8fb7b3ea7c51523ec070c
kk-github2 <kalai.kumar1504@yahoo.com>
2024-03-09 17:35:10 +1100

    Pilot commit

---
+++ cypress/e2e/test1.js
@@ -0,0 +1,12 @@
+describe ('My first test', function()
+{
+it('Visits the new logo search page', function()
+{
+    cy.visit('https://https://www.brandcrowd.com/maker/mydesigns/logodrafts/00c784ed-daa2-406b-9d37-9a9576dffef9/templatetypes')
+}
+
+)
+
+}
+
+)
---
+++ cypress/integration/examples/1OpenLogoSearch.ts
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+describe ('Pilot test', function()
+{
+it('Open the new logo search page', function()
+{
+    cy.visit('https://www.brandcrowd.com/maker/mydesigns/logodrafts/00c784ed-daa2-406b-9d37-9a9576dffef9/templatetypes')
+}
+
+)
+
+}
+
+)
