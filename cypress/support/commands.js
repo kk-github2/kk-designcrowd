2d286744c44a701fb5f70099758a688ced51e4a9
kk-github2 <kalai.kumar1504@yahoo.com>
2024-03-10 12:26:18 +1100

    Commit continue

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,34 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// cypress/support/commands.ts
+
+// typescript
+// cypress/support/commands.ts
+
+// Custom Cypress command to click 'My account' on www.brandcrowd.com
+Cypress.Commands.add('clickMyAccount', () => {
+    cy.get('#__layout > div > div > div.o-main > section:nth-child(2) > div > div:nth-child(2) > div > div > div.swiper-container.swiper-container-initialized.swiper-container-horizontal > div > div.swiper-slide.swiper-slide-active > div > section > a > figure > img').click();
+   });
+
+/*
+cy.visit('https://www.brandcrowd.com')
+    cy.get('.tw-cursor-pointer').contains(' My Account ').click({force: true});
+    cy.get('input[type=email]').type('tech-challenge@kk.com')
+    cy.get('.tw-w-full').contains('Login').click({force: true});
+    cy.get('input[type=password]').type('Challenge1')
+    cy.get('.tw-w-full').contains('Login').click({force: true});
+Commands
+Cypress.Commands.login = (username, password) => {
+    cy.get('.tw-cursor-pointer').contains(' My Account ').click({ force: true });
+    cy.get('input[type=email]').type('tech-challenge@kk.com')
+    cy.get('.tw-w-full').contains('Login').click({ force: true });
+    cy.get('input[type=password]').type('Challenge1')
+    cy.get('.tw-w-full').contains('Login').click({ force: true });
+
+}
+
+Cypress.Commands.launchPage = (url) => {
+    cy.visit('https://www.brandcrowd.com')
+}
+*/
